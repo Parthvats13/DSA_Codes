55c5927da737643b0793d31f7e30689afc72fa42
Parthvats13 <167945184+Parthvats13@users.noreply.github.com>
2024-09-07 17:57:29 +0530

    Create Binary_Search.cpp

---
+++ Binary_Search.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int  arr[]={1,3,6,13,17,19,24,29,30,32,35};
+  int target;
+  cout<< "enter target \n";
+  
+  cin>> target;
+  int lb=0;
+  int ub=10;
+  int flag=0;
+  int mid=(lb+ub+2)/2;
+  while(lb<=ub)
+    {
+      mid=(lb+ub+2)/2;
+      if(arr[mid]==target)
+      { flag=1;
+         break;
+      }
+      else if(arr[mid]<target)
+      {
+        
+        lb = mid+1;
+      }
+      else if(arr[mid]>target)
+      {
+      
+        ub = mid - 1;
+      }
+      
+    }
+  if(flag==0)
+    cout<< "element not found";
+  else 
+    cout<<"element found at "<< mid <<" index\n";
+  
+  }
